5eb6393f6f0e6989a88a84188fd28717c8284d57
S. Tarun Kumar <tarunkumar051005@gmail.com>
2025-11-12 23:17:27 +0530

    eslint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
 
-module.exports = nextConfig
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+ 
+
+
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://10.19.1.78:3000",
+  ],
+
+  
+  outputFileTracingRoot: __dirname,
+
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
